529fc63960d3a653cc90a0898e89aeb54dfb3b0c
Orzech <ozh204@gmail.com>
2016-12-27 18:26:15 +0100

    poprawki, przykład zły

--- scripts/runHSQLDBServer.sh
+++ scripts/runHSQLDBServer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -cp ./scripts/hsqldb-2.2.4.jar org.hsqldb.server.Server --database.0 mem:mydb --dbname.0 workdb
+java -cp ./hsqldb-2.2.4.jar org.hsqldb.server.Server --database.0 mem:mydb --dbname.0 workdb
